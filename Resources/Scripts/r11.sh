c380fe16ad122721c02fccd8d3d3654e0d892504
XiKoTaSu <3329989998@qq.com>
2026-05-02 14:42:56 +0800

    fix

--- Resources/Scripts/r11.sh
+++ Resources/Scripts/r11.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 # Build an Android kernel that is actually UEFI disguised as the Kernel
-cat ./BootShim/BootShim.bin "./Build/r11Pkg/${TARGET_BUILD_MODE}_CLANGPDB/FV/r11_UEFI.fd" > "./Build/r11Pkg/${TARGET_BUILD_MODE}_CLANGPDB/FV/r11_UEFI.fd-bootshim"||exit 1
-gzip -c < "./Build/r11Pkg/${TARGET_BUILD_MODE}_CLANGPDB/FV/r11_UEFI.fd-bootshim" > "./Build/r11Pkg/${TARGET_BUILD_MODE}_CLANGPDB/FV/r11_UEFI.fd-bootshim.gz"||exit 1
-cat "./Build/r11Pkg/${TARGET_BUILD_MODE}_CLANGPDB/FV/r11_UEFI.fd-bootshim.gz" ./Resources/DTBs/r11.dtb > ./Resources/bootpayload.bin||exit 1
+cat ./BootShim/BootShim.bin "./Build/r11Pkg/${TARGET_BUILD_MODE}_CLANGPDB/FV/R11_UEFI.fd" > "./Build/r11Pkg/${TARGET_BUILD_MODE}_CLANGPDB/FV/R11_UEFI.fd-bootshim"||exit 1
+gzip -c < "./Build/r11Pkg/${TARGET_BUILD_MODE}_CLANGPDB/FV/R11_UEFI.fd-bootshim" > "./Build/r11Pkg/${TARGET_BUILD_MODE}_CLANGPDB/FV/R11_UEFI.fd-bootshim.gz"||exit 1
+cat "./Build/r11Pkg/${TARGET_BUILD_MODE}_CLANGPDB/FV/R11_UEFI.fd-bootshim.gz" ./Resources/DTBs/r11.dtb > ./Resources/bootpayload.bin||exit 1
 
 # Create bootable Android boot.img
 python3 ./Resources/Scripts/mkbootimg.py \
